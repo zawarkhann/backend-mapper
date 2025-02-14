74aa0b15700b4f121933c8169a8a697fef6f5a33
zawarkhann <zawark807@gmail.com>
2025-02-14 17:02:14 +0500

    node removed

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 const { google } = require("googleapis");
-require('dotenv').config();
 const app = express();
 
 
@@ -23,18 +22,19 @@ const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
 
 const auth = new google.auth.GoogleAuth({
     credentials: {
-        type: process.env.TYPE,
-        project_id: process.env.PROJECT_ID,
-        private_key_id: process.env.PRIVATE_KEY_ID,
-        private_key: process.env.PRIVATE_KEY,
-        client_email: process.env.CLIENT_EMAIL,
-        client_id: process.env.CLIENT_ID,
-        auth_uri: process.env.AUTH_URI,
-        token_uri: process.env.TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-        universe_domain: process.env.UNIVERSE_DOMAIN
+        type: "service_account",
+        project_id: "mapper-450909",
+        private_key_id: "f6bb29c87b6382250723cc8e66122d33e73da27e",
+        private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCkZWgOnmWIOePO\n/pn0N365id6Y2oMMgXQ3Q+BMMbLYLsh8k4TRS0o2ESR5c0YPiSHA4/AUzfTSZbKg\nmVSCkV2bnFt0gzkGbNDgePGVFyWk9XkYNsNv3cdpl1lwYxqz4M/UoHOnueiRfAun\nwM/T7UkipCOPbhM6A9ayTmemgymgK8jCExkOdt/g2FawEK09rXMVofdN8dqtLBz4\nvM8nyIFdKETwVFdslPXWMZGwRmozD4O4Jwl5jC0kdtzcYqgWG60GUqdmpUTkIXAN\nxAnDpX+C4+gxIjz9wwRPKRePgnRagbZGwwm7m45OlOSMHrliFNmlCc5F1ockaKL9\nmYdFicT9AgMBAAECggEAAj7zFFAMVGIbafaHJT5PguVj9HP3xQdNDnw6POWTlenT\nCgX8kzITdM2jJphGkBZwLvHDk+UvFQ4X9KMOSS0HObBXJ4OrbG+2zXpELU9AT71I\nHDvooPHyCXqXLuE2ZNgLaXzlAkOYmR7itzX7y4oQV7glAY6gFfN7eWV3e7TgWrR4\nz04RTDYjYr45MIrFaqwH18S2h43xvVWaKh3+Srvj1F8AmCvGgL15w17t5f4r2qed\nIktMkw95ke/HYljJ7gwxQAsjm634gXYkydgv8lV83R7kt8ERwbGGOnGpn7H4gr1N\nM8v21Z81Kq7giB0Uqp0GmEATVsxOK7vZyCXIYl9QGQKBgQDaLz3P3hjTe2JKkLds\nzb/8DkjmkRPiJrwvcFSWq9MKi34IlCJtd5XSpO64mlqerp34dTlI9ANMPp3JklPF\nQ8m0wXwr0xiR5YoxNRQkDhXFFn8f1UYagamDEppMDfMlSvRwDEtojjriCJ069+Es\nINvFZ1HynPoQn2BWjjsmSnuZlwKBgQDA45hOckq4domzRCYtiN2v4+y1i74mGTXu\nwbztvp+GYAJoDrLFBTSOpksQGed0W/54m3E4ghbmNnmUTT/LtU+xSqK79jgz0vG3\ns55rkI16xg23ZjbRcdI8bri2Jl6kcfbksvxPVHE4yj5qZFDFkxYKiTOvwh/a9tNB\nMGYKxumgiwKBgFHsqykCHBBMo/ubm0QffZcHRhhNIUopgT/OzfRFUFkFR62w8bxo\nyrjtdTWbv6sVpftFCN17wkE+wcCOUSYg7fwRKq7J9M/TgLA5Xfar00DN0a4CRvBs\n/1lYAtfL5SB7pIzuEOKRhUqaMW+S49nN/LeCtCKFDuBu952Iim02SnLBAoGAOVny\niTCWYu5r1u9mYpeVQp0EXp5kNTO9w3W7VP0etXeKf6IPKWF41QlAvFLXbNOFgBrH\nuUOEx4Je9+dYFpucTFyisjxtTYtk73fqnJToXB7TKTqtBe1P00Qgi5gw/7ga7rkB\nKB01ZOtNT5jwIy6umQdeZN3VVueYvINFc+LcyC0CgYBHFoF9AIwI5ed2g4HRfMk5\ndxb8khbj9JcoAoy5yiXywcI4+7n3pX0CtCB3cUsDzD3+7THl86t8P/Jkz3FDMawp\nXxelP4Z5rPVOX0Rvk8FIyRBkPfXoSd51Lu3kyNCC9iA+g6wS40M5Uidl0dn7yKJ9\nbPlayDi310a9toGLLJ0PAA==\n-----END PRIVATE KEY-----\n",
+        client_email: "mapper-video@mapper-450909.iam.gserviceaccount.com",
+        client_id: "100665152441658092546",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/mapper-video%40mapper-450909.iam.gserviceaccount.com",
+        universe_domain: "googleapis.com"
     },
+   
     scopes: SCOPES,
 });
 
@@ -79,8 +79,9 @@ app.post("/upload", upload.single('file'), async (req, res) => {
     }
 });
 
-app.listen(5050, () => {
-    console.log('Server running on port 5050');
-});
+// app.listen(5050, () => {
+//     console.log('Server running on port 5050');
+// });
 
-// Install dependencies: npm install express multer googleapis cors
+
+module.exports = app;
