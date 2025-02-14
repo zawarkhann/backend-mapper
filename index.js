2959816f6a033b3fb55e13453bf824913f72f821
zawarkhann <zawark807@gmail.com>
2025-02-14 16:17:56 +0500

    Firdt commit

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 const { google } = require("googleapis");
+require('dotenv').config();
 const app = express();
+
 
 // Enable CORS for all origins, inmf cmf cmf cnf cmf cmf cmf cm fmc fmc fmcluding localhost
 app.use(cors());
@@ -21,7 +23,19 @@ const KEYFILEPATH = path.join(__dirname, "cred.json");
 const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
+    credentials: {
+        type: process.env.TYPE,
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+        universe_domain: process.env.UNIVERSE_DOMAIN
+    },
     scopes: SCOPES,
 });
 
